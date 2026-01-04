9ab6acd988d2fda4da656036677661862e5ae826
Sharath282000 <sharathmurugan28@gmail.com>
2026-01-05 00:09:29 +0530

    Added the MetaData and search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { Analytics } from '@vercel/analytics/next';
 export const heebo = Heebo({
   subsets: ['latin'],
   weight: [
-    '100','200','300','400','500',
-    '600','700','800','900'
+    '100', '200', '300', '400', '500',
+    '600', '700', '800', '900'
   ],
   variable: '--font-heebo',
   display: 'swap',
@@ -18,8 +18,8 @@ export const heebo = Heebo({
 export const inter = Inter({
   subsets: ['latin'],
   weight: [
-    '100','200','300','400','500',
-    '600','700','800','900'
+    '100', '200', '300', '400', '500',
+    '600', '700', '800', '900'
   ],
   variable: '--font-inter',
   display: 'swap',
@@ -27,14 +27,59 @@ export const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Sharath - Portfolio",
-  description: "This website shows all my works, short bio and updated contact information",
-  icons:{
-    icon :{
-      url : "/icon.png",
+  description: "Building full-stack products and secure cloud systems. Explore my works and journey.",
+  keywords: [
+    "Sharath",
+    "Full Stack Developer",
+    "Cloud Security Analyst",
+    "Deloitte",
+    "Portfolio",
+    "Web Development",
+    "Website",
+    "Chennai",
+    "Oracle Analyst",
+    "Deloitte Analyst",
+    "Analyst",
+    "Sharath - Portfolio",
+    "Sharath Portfolio",
+    "Sharath space",
+    "Space"
+  ],
+  authors: [{ name: "Sharath" }],
+  creator: "Sharath",
+  metadataBase: new URL("https://sharath.online"),
+  icons: {
+    icon: {
+      url: "/icon.png",
       sizes: "512x512",
       type: "image/png",
     }
-  }
+  },
+  openGraph: {
+    title: "Sharath - Portfolio",
+    description:
+      "Building full-stack products and secure cloud systems. Explore my works and journey.",
+    url: "https://sharath.online",
+    siteName: "Sharath",
+    images: [
+      {
+        url: "/icon.png",
+        width: 1200,
+        height: 630,
+        alt: "Sharath Portfolio",
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Sharath | Full-Stack Developer & Cloud Security Analyst",
+    description:
+      "Projects, skills, and thoughts on building modern web experiences.",
+    creator: "@Sharath_tech",
+    images: ["/icon.png"],
+  },
 };
 
 export default function RootLayout({
@@ -49,8 +94,8 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
-        <Socialdock/>
-        <Footer/>
+        <Socialdock />
+        <Footer />
       </body>
     </html>
   );
