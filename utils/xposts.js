946dd3879b3720c773217f8561bcb0cfad838e88
M <sharathm@deloitte.com>
2026-02-02 13:57:48 +0530

    Posts update

--- utils/xposts.js
+++ utils/xposts.js
@@ -1,18 +1,18 @@
 export const posts = [
   {
+    id: 1,
+    description:
+      "Thread: How I use AI while coding (without vibe coding)\n 1/10\n AI is part of my coding workflow now.\n But I don’t “vibe code.”\n Here’s how I actually use AI, and where I draw the line 👇",
+    link: "https://x.com/Sharath_tech/status/2012305950598906170",
+    date: "17 Jan, 2026",
+    tags: "#Learning, #AI, #Coding",
+  },
+  {
     id: 2,
     description:
       "Using an external wide screen monitor makes coding feel easier.\n One screen for writing code, the other for docs and previews.\n Fewer distractions, less context switching, and better focus during long sessions.\n Always in love with improving my work setup ❤️\n #desksetup #monitor",
     link: "https://x.com/Sharath_tech/status/2009256277264716236",
     date: "08 Jan, 2026",
     tags: "#Coding, #Setup",
-  },
-  {
-    id: 1,
-    description:
-      "I’ve noticed something.\n I work every day and stay busy,but I’m not learning as much as I want to.\n It’s not a motivation problem. It’s all about time.\n Learning happens only “if I have energy left” - and most days, I don’t.\n Time to fix my routine and make learning consistent.",
-    link: "https://x.com/Sharath_tech/status/2009007653977194799",
-    date: "08 Jan, 2026",
-    tags: "#Learning, #Thoughts",
   },
 ];
